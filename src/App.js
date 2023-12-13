1bdc00bf10e64add2e8f6db827c452e011933908
Vanessa Ehrlein <vanessa.ehrlein@posteo.de>
2023-12-13 12:21:18 +0100

    add classNames

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import "./styles.css";
 
 export default function App() {
   return (
-  <article>
-    <h2 className="article__title">
-      My new article
-    </h2>
-    <label htmlFor="article__check">Check when you have read this article</label>
-    
-    <input id="article__check" name="article__check" type="checkbox"></input>
-    <br></br>
-    <br></br>
-    <a className="article__llink" href="https://de.wikipedia.org/wiki/Wichtel" aria-label="Link to an article about Wichtel">Link to an article about Wichtel/ Gnomes </a>
-  </article>
+  <NewArticle/>
   );
+}
+
+function NewArticle() {
+  return (
+    <article className="article">
+      <h2 className="article__title">
+        My new article
+      </h2>
+      <label htmlFor="article__check">Check when you have read this article</label>
+      
+      <input id="article__check" name="article__check" type="checkbox"></input>
+      <br></br>
+      <br></br>
+      <a className="article__link" href="https://de.wikipedia.org/wiki/Wichtel" aria-label="Link to an article about Wichtel">Link to an article about Wichtel/ Gnomes </a>
+    </article>
+    );
 }
