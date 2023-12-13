872fdb69650db35bc23550332612b9c85313f0df
Vanessa Ehrlein <vanessa.ehrlein@posteo.de>
2023-12-13 11:40:11 +0100

    created new article

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return (
+  <article>
+    <h2 className="article__title">
+      My new article
+    </h2>
+    <label htmlFor="article__check">Check when you have read this article</label>
+    
+    <input id="article__check" name="article__check" type="checkbox"></input>
+    <br></br>
+    <br></br>
+    <a className="article__llink" href="https://de.wikipedia.org/wiki/Wichtel" aria-label="Link to an article about Wichtel">Link to an article about Wichtel/ Gnomes </a>
+  </article>
+  );
 }
